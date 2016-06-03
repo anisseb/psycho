941104e9936180d14a82a704cee12b6050892fc1
PRAS Martin <martin.pras@epitech.eu>
2016-06-03 19:17:46 +0200

    -html2pdf + dompdf

--- start/script.php
+++ start/script.php
@@ -1,26 +1,20 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Codes d'accès au serveur central de la NASA</title>
-    </head>
-    <body>
-    
-        <?php
+<?php
+require_once 'vendor/dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
 
-        $result = 0;
-        $forfait = "";
-        $result_consulting = 0;
-        $result_conseil = 0;
-        $result_recrutement = 0;
-        $result_reiteration = 0;
-        $result_conseil = 0;
-        $result_management = 0;
-        $result_coW = 0;
-        $value_cw = 0;
-        $result = 0;
-        $tva = 0;
-        $result_ttc = 0;
+    $result = 0;
+    $forfait = "";
+    $result_consulting = 0;
+    $result_conseil = 0;
+    $result_recrutement = 0;
+    $result_reiteration = 0;
+    $result_conseil = 0;
+    $result_management = 0;
+    $result_coW = 0;
+    $value_cw = 0;
+    $result = 0;
+    $tva = 0;
+    $result_ttc = 0;
 
     if (isset($_POST['consultant']) && isset($_POST['consultant_day']))
     {
@@ -63,112 +57,109 @@
     $result_ttc = $result * 1.20;
     $tva = $result_ttc - $result;
 
-    ?>
-    <p><strong>Résultat HT = <?php echo $result ?> Euros</strong></p>
-    <p><strong>Résultat TVA = <?php echo $tva?> Euros</strong></p>
-    <p><strong>Résultat TTC = <?php echo $result_ttc ?> Euros</strong></p>
-    <p><strong>Résultat Consulting = <?php echo $result_consulting ?> Euros HT</strong></p>
-    <p><strong>Résultat Connseil RH = <?php echo $result_conseil ?> Euros HT</strong></p>
-    <p><strong>Résultat Staffing = <?php echo $result_recrutement ?> Euros HT</strong></p>
-    <p><strong>Résultat Réiteration = <?php echo $result_reiteration ?> Euros HT</strong></p>
-    <p><strong>Résultat Management = <?php echo $result_management ?> Euros HT</strong></p>
-    <p><strong>Résultat Co - Working = <?php echo $result_coW ?> Euros HT</strong></p>
-    <p><strong>FORFAIT <?php print($forfait) ?></strong></p>
-<<<<<<< HEAD
-</html>
-<?php
-$content='<page><html>
-<head>
+$content = '<html>
+                <head>
+                    <meta charset="utf-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <meta name="description" content="">
+                    <meta name="author" content="">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+                    <title>Folks</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/pdf.css" rel="stylesheet">
-    <!-- Custom CSS -->
+                    <!-- Bootstrap Core CSS -->
+                    <link href="css/bootstrap.min.css" rel="stylesheet">
+                  <link href="css/pdf.css" rel="stylesheet">
+                    <!-- Custom CSS -->
 
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+                    <!-- Custom Fonts -->
+                    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+                    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+                    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+                    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+                    <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->
+                    <!--[if lt IE 9]>
+                        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+                        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+                    <![endif]-->
 
 
-<h1 class="titre">folks</h1>
-<div class="entete">BUMPER FRANCE
-    <br> 63, rue de Créqui
-    <br>69006 LYON
-</div>
-<div class="entete">LYON, LE 18 AVRIL 2016
-        <br>OFFRE : BU-01-16
-</div>
-</head>
-<body style="background-color: #EFEFEF;">
-<h2 class="recrutement">recrutement dans le secteur de la communication</h2>
-<h3 class="titreservice">consulting</h3>
-<div class="texte">
-</div>
-<span class="prix">1890,00 € HT</span>
-<h3 class="titreservice">conseil rh</h3>
-<div class="texte"></div>
-<span class="prix">630,00 € HT</span>
-<h3 class="titreservice">staffing</h3>
-<div class="texte"></div>
-<span class="prix">630,00 € HT</span>
-<div class="rem">rémunération du personnel a prevoir en sus</div>
-<h3 class="titreservice">réitération de contrat</h3>
-<div class="texte"></div>
-<span class="prix">315,00 € HT</span>
-<div class="texte"></div>
-<span class="prix">630,00 € HT</span>
-<div class="texte">cdi</div>
-<span class="prix">1890,00 € HT</span>
-<h3 class="titreservice">management</h3>
-<div class="texte">
-</div>
-<span class="prix">1440, 00 € HT</span>
-<h3 class="titreservice">coworking</h3>
-<div class="texte">
-</div>
-<span class="prix">900,00 € HT</span>
-</body>
-<footer>
-<div class="textefooterl">
-FOLKS 
-<br>SARL AU CAPITAL DE 25.000 EUROS
-
-<br>64, RUE MOLIERE – 69003 LYON 
-<br>04.27.01.46.20
-<br>CONTACT@FOLKS.FR
-<br>WWW.FOLKS.FR
-
-<br>RCS LYON : SIRET : 525137675
-<br>TVA INTRA : FR 50525137675 
-</div>
-<div class="textefooterr">OFFRE VALABLE UN MOIS
- <br>BON POUR ACCORD / SIGNATURE / CACHET
- <br>PAIEMENT DE 30 % A LA COMMAINDE
-<br>PAIEMENT 70 % A LA PRISE DE FONCTION</div>
-</footer>
-</html></page>';
+                <h1 class="titre">folks</h1>
+                <div class="entete">BUMPER FRANCE
+                  <br> 63, rue de Créqui
+                  <br>  69006 LYON
+                </div>
+                <div class="entete">LYON, LE 18 AVRIL 2016
+                    <br>OFFRE : BU-01-16
+                </div>
+                </head>
+                <body style="background-color: #EFEFEF;">
+                <h2 class="recrutement">recrutement dans le secteur de la communication</h2>
+                <h3 class="titreservice">consulting</h3>
+                <div class="texte">mise en place d\'une stratégie de web-marketing avec pour objectifs : 
+                  <br>augmentation du trafic du site internet, acquisitions de nouveaux mandats et 
+                  <br>  recherche de clients potentiels
+                </div>
+                <div class="prix">'.$result_consulting.',00 € HT</div>
+                <h3 class="titreservice">conseil rh</h3>
+                <div class="texte">mise en place d\'un ordre de mission et d\'une feuille de route</div>
+                <div class="prix">'.$result_conseil.',00 € HT</div>
+                <h3 class="titreservice">staffing</h3>
+                <div class="texte">identification, contrôle et mise à disposition
+                  <br>d\'un profil junior sélectionné en fonction de ses disponibilités, de 
+                  <br>  ses compéteneces, de son parcours et de sa personnalité pendant 6 mois
+                </div>
+                <div class="prix">'.$result_recrutement.',00 € HT</div>
+                <div class="rem">rémunération du personnel a prevoir en sus</div>
+                <h3 class="titreservice">réitération de contrat</h3>
+                <div class="texte">stage et cdd de -6 mois</div>
+                <div class="prix">'.$result_reiteration.',00 € HT</div>
+                <h3 class="titreservice">management</h3>
+                <div class="texte">24 points hebdomadaires
+                  <br>3 réunions physiques (69) bimensuelles de deux heures
+                </div>
+                <div class="prix">'.$result_management.', 00 € HT</div>
+                <h3 class="titreservice">coworking</h3>
+                <div class="texte">mise à disposition d\'un bureau (69) sur 6 mois
+                </div>
+                <div class="prix">'.$result_coW.',00 € HT</div>
+                </body>
 
 
- 
-require_once(__DIR__ . '/html2pdf/vendor/autoload.php');
-    $html2pdf = new HTML2PDF('l','A4','fr','ISO-8859-15');
-    $html2pdf->WriteHTML($content);
-    /*$html2pdf->Output(__DIR__ . "/testage5.pdf", 'F');*/
-    /*$html2pdf->Output("testage.pdf");*/
+                <footer>
+                <div class="textefooterl">
+                FOLKS 
+                <br>SARL AU CAPITAL DE 25.000 EUROS
+
+                <br>64, RUE MOLIERE – 69003 LYON 
+                <br>04.27.01.46.20
+                <br>CONTACT@FOLKS.FR
+                <br>WWW.FOLKS.FR
+
+                <br>RCS LYON : SIRET : 525137675
+                <br>TVA INTRA : FR 50525137675 
+                </div>
+                <div class="textefooterr">OFFRE VALABLE UN MOIS
+                 <br>BON POUR ACCORD / SIGNATURE / CACHET
+                 <br>PAIEMENT DE 30 % A LA COMMAINDE
+                <br>PAIEMENT 70 % A LA PRISE DE FONCTION</div>
+                </footer>
+            </html>';
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->set_option('isHtml5ParserEnabled', true);
+$dompdf->loadHtml($content);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'portrait');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream('my.pdf',array('Attachment'=>0));
 
   /*  $destinataire = 'martin.pras@epitech.eu';
     // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
@@ -195,4 +186,4 @@ require_once(__DIR__ . '/html2pdf/vendor/autoload.php');
         return true;
     }
 }*/
-?></html>
+?>
---
+++ start/vendor/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,93 @@
+<?php return array (
+  'sans-serif' => array(
+    'normal' => $fontDir . '\Helvetica',
+    'bold' => $fontDir . '\Helvetica-Bold',
+    'italic' => $fontDir . '\Helvetica-Oblique',
+    'bold_italic' => $fontDir . '\Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $fontDir . '\Helvetica',
+    'bold' => $fontDir . '\Helvetica-Bold',
+    'italic' => $fontDir . '\Helvetica-Oblique',
+    'bold_italic' => $fontDir . '\Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $fontDir . '\ZapfDingbats',
+    'bold' => $fontDir . '\ZapfDingbats',
+    'italic' => $fontDir . '\ZapfDingbats',
+    'bold_italic' => $fontDir . '\ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $fontDir . '\Symbol',
+    'bold' => $fontDir . '\Symbol',
+    'italic' => $fontDir . '\Symbol',
+    'bold_italic' => $fontDir . '\Symbol',
+  ),
+  'serif' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $fontDir . '\DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '\DejaVuSans-Oblique',
+    'normal' => $fontDir . '\DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $fontDir . '\DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '\DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '\DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $fontDir . '\DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '\DejaVuSerif-Italic',
+    'normal' => $fontDir . '\DejaVuSerif',
+  ),
+  'glyphicons halflings' => array(
+    'normal' => $fontDir . '\f5e6c0fa76d3288fcebc81f4e391b126',
+  ),
+  'fontawesome' => array(
+    'normal' => $fontDir . '\a10d66a48a60338df014f73be0752be6',
+  ),
+  'lora' => array(
+    'normal' => $fontDir . '\687fcc8dddeee574dc18d614e8adcb30',
+    'italic' => $fontDir . '\84b34e0fad203950223c6d6ebbad1626',
+  ),
+  'montserrat' => array(
+    'normal' => $fontDir . '\3948d10e3ab89a0d5d2e5417895217bd',
+  ),
+) ?>
